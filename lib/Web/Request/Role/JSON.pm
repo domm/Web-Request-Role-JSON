6754c6972b128a1be1dcfe3642b75cd0a8f3e662
Thomas Klausner <domm@plix.at>
2017-01-20 16:59:49 +0100

    documentation

--- lib/Web/Request/Role/JSON.pm
+++ lib/Web/Request/Role/JSON.pm
@@ -76,13 +76,65 @@ sub new_json_error {
   extends 'Web::Request';
   with 'Web::Request::Role::JSON';
 
-  # Make sure your app uses your Request class, e.g. using OX:
+  # Make sure your app uses your request handler, e.g. using OX:
   package My::App::OX;
   sub request_class {'My::App::Request'}
 
   # Finally, in some controller action
+  sub create_POST {
+      my ($self, $req) = @_;
+
+      my $data    = $req->decoded_json_content;
+      my $created = $self->model->create($data);
+      return $self->new_json_response($created, undef, 201);
+  }
 
 =head1 DESCRIPTION
+
+C<Web::Request::Role::JSON> provides a few methods that make handling
+JSON in L<Web::Request> a bit easier.
+
+Please note that all methods return a L<Web::Response> object.
+Depending on the framework you use (or lack thereof), you might have
+to call C<finalize> on the response object to turn it into a valid
+PSGI response.
+
+=head2 METHODS
+
+=head3 decoded_json_content
+
+  my $perl_hash = $req->decoded_json_content;
+
+Extracts and decodes a JSON payload from the request.
+
+=head3 new_json_response
+
+  $req->new_json_response( $data );
+  $req->new_json_response( $data, $header_ref );
+  $req->new_json_response( $data, $header_ref, $http_status );
+
+Convert your data to JSON and generate a new response with correct HTTP headers.
+
+You can pass in more headers as the second argument (either hashref or
+arrayref). These headers will be passed straight on to
+C<< HTTP::Headers->new() >>.
+
+You can also pass a HTTP status code as the third parameter. If none
+is provided, we default to C<200>.
+
+=head3 new_json_error
+
+  $req->new_json_response( 'something is wrong' );
+  $req->new_json_response( $error_data );
+  $req->new_json_response( $error, $status );
+
+Generate a JSON object out of your error message, if the message is a
+plain string. But you can also pass in a data structure that will be
+converte to JSON.
+
+Per default, HTTP status is set to C<400>, but you can pass any other
+status as a second argument. (Yes, there is no checking if you pass a
+valid status code or not. You're old enough to not do stupid things..)
 
 =head1 THANKS
 
